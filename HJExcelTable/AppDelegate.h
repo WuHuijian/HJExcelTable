42a90933f18ece7b53dd4e24a27760126fed147c
WHJ <wuhuijian@weibangong.com>
2019-09-05 11:48:43 +0800

    Initial Commit

---
+++ HJExcelTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HJExcelTable
+//
+//  Created by WHJ on 2019/9/5.
+//  Copyright © 2019 WHJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
